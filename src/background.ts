412a67380641d488e45d51b858e60cba4f749e8f
danie <danieljavierguzzi@gmail.com>
2024-02-04 19:55:33 -0300

    Fixed adding listener on non backloggd pages

--- src/background.ts
+++ src/background.ts
@@ -1,21 +1,14 @@
 chrome.runtime.onMessage.addListener(
     (url, sender, onSuccess) => {
-        fetch(url, {
-            method: "GET",
-            headers: {
-                'Accept': 'application/json',
-                'Content-Type': 'application/json'
-            }
-        }).then(response => response.json()).then(value => onSuccess(value));
-
+        fetch(url).then(response => response.json()).then(value => onSuccess(value));
         return true;
     }
 );
 
-chrome.tabs.onUpdated.addListener((tabId, changeInfo) => {
-    if (changeInfo.status === 'complete') {
+chrome.tabs.onUpdated.addListener((tabId: number, changeInfo: chrome.tabs.TabChangeInfo, tab: chrome.tabs.Tab) => {
+    if (tab?.url?.match('https:\/\/.*.backloggd.com\/.*') && changeInfo.status === 'complete') {
         chrome.tabs.sendMessage(tabId, {
             message: 'TabUpdated'
-        });
+        }).then();
     }
 });
