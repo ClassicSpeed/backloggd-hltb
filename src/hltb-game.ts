5d5bf3d25f43875a547ebb88b3cf999df86baa1c
danie <danieljavierguzzi@gmail.com>
2024-02-05 22:02:51 -0300

    Improved project architecture

---
+++ src/hltb-game.ts
@@ -0,0 +1,48 @@
+type HLTBResponse = HLTBData[];
+type HLTBData = {
+    gameName: string,
+    beatTime: {
+        main: { avgSeconds: number },
+        extra: { avgSeconds: number },
+        completionist: { avgSeconds: number },
+        all: { avgSeconds: number }
+    }
+};
+
+class HLTBGame {
+    constructor(private data: HLTBData) {
+        this.data = data;
+    }
+
+    get gameName(): string {
+        return this.data.gameName;
+    }
+
+    get mainBeatTime(): string {
+        return formatHours(this.data.beatTime.main.avgSeconds / 3600);
+    }
+
+    get extraBeatTime(): string {
+        return formatHours(this.data.beatTime.extra.avgSeconds / 3600);
+    }
+
+    get completionistBeatTime(): string {
+        return formatHours(this.data.beatTime.completionist.avgSeconds / 3600);
+    }
+
+    get allBeatTime(): string {
+        return formatHours(this.data.beatTime.all.avgSeconds / 3600);
+    }
+}
+
+function formatHours(hours: number, tolerance: number = 0.1): string {
+    const roundedHours = Math.round(hours * 2) / 2;
+    const decimalPart = roundedHours % 1;
+
+    if (Math.abs(decimalPart - 0.5) < tolerance && hours < 10) {
+        return `${Math.floor(roundedHours)}½`;
+    } else {
+        return `${Math.floor(roundedHours)}`;
+    }
+}
+
--- src/time-badge.ts
+++ src/time-badge.ts
@@ -30,7 +30,7 @@ function addTimeBadges() {
             "https://hltb-proxy.fly.dev/v1/query?title=" + gameTitle,
             function (response: HLTBResponse) {
                 if (response.length > 0 && response[0].beatTime.main.avgSeconds > 0) {
-                    addBadge(gameCover as HTMLElement, response[0]);
+                    addBadge(gameCover as HTMLElement, new HLTBGame(response[0]));
                 }
             }
         );
@@ -38,41 +38,19 @@ function addTimeBadges() {
     });
 }
 
-function addBadge(parentElement: HTMLElement, hltbData: HLTBData) {
+function addBadge(parentElement: HTMLElement, hltbGame: HLTBGame) {
     const badgeDiv = document.createElement('div');
     badgeDiv.classList.add('time-badge');
-    badgeDiv.innerText = formatHours(hltbData.beatTime.main.avgSeconds / 3600) + ' h';
-    badgeDiv.title = hltbData.gameName
-        + '\n\nMain Story: ' + formatHours(hltbData.beatTime.main.avgSeconds / 3600) + ' Hours'
-        + '\nMain + Sides: ' + formatHours(hltbData.beatTime.extra.avgSeconds / 3600) + ' Hours'
-        + '\nCompletionist: ' + formatHours(hltbData.beatTime.completionist.avgSeconds / 3600) + ' Hours'
-        + '\nAll Styles: ' + formatHours(hltbData.beatTime.all.avgSeconds / 3600) + ' Hours';
+    badgeDiv.innerText = hltbGame.mainBeatTime + ' h';
+    badgeDiv.title = `${hltbGame.gameName}`
+        + `\n- Main Story: ${hltbGame.mainBeatTime} Hours`
+        + `\n- Main + Sides: ${hltbGame.extraBeatTime} Hours`
+        + `\n- Completionist: ${hltbGame.completionistBeatTime} Hours`
+        + `\n- All Styles: ${hltbGame.allBeatTime} Hours`;
     parentElement.appendChild(badgeDiv);
 }
 
 
-function formatHours(hours: number, tolerance: number = 0.1): string {
-    const roundedHours = Math.round(hours * 2) / 2;
-    const decimalPart = roundedHours % 1;
-
-    if (Math.abs(decimalPart - 0.5) < tolerance && hours < 10) {
-        return `${Math.floor(roundedHours)}½`;
-    } else {
-        return `${Math.floor(roundedHours)}`;
-    }
-}
-
-
-type HLTBResponse = HLTBData[];
-type HLTBData = {
-    gameName: string,
-    beatTime: {
-        main: { avgSeconds: number },
-        extra: { avgSeconds: number },
-        completionist: { avgSeconds: number },
-        all: { avgSeconds: number }
-    }
-};
 
 
 
